c259c693df8f68549fbbc608a95e2afe7f25f812
hafizflow <hafizur.rahman.cs@gmail.com>
2025-08-01 11:46:35 +0600

    Initial Commit

---
+++ Practice/PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PracticeApp.swift
+//  Practice
+//
+//  Created by Hafizur Rahman on 1/8/25.
+//
+
+import SwiftUI
+
+@main
+struct PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
